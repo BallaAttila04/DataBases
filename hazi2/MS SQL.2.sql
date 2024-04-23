e97b29bc56b9e5c74a1b1ae71ddcfa73be51e917
BallaAttila <57375478+BallaAttila04@users.noreply.github.com>
2024-04-23 21:17:41 +0200

    Add files via upload

---
+++ hazi2/MS SQL.2.sql
@@ -0,0 +1,2 @@
+CREATE USER maszkolthazi_user WITHOUT login;
+Grant select on Vendeg_hazi to maszkolthazi_user
---
+++ hazi2/MS SQL.3.sql
@@ -0,0 +1,4 @@
+Execute as User='maszkolthazi_user';
+SELECT * from Vendeg_hazi
+revert
+SELECT * from Vendeg_hazi
